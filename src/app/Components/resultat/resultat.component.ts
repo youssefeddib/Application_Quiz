8b1d96a916ddc3044ffac00510dedefe1b8b382a
youssef eddib <eddiby79@gmail.com>
2025-04-15 16:29:02 +0200

    Ceration HTML de Component Home

---
+++ src/app/Components/resultat/resultat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resultat',
+  standalone: true,
+  imports: [],
+  templateUrl: './resultat.component.html',
+  styleUrl: './resultat.component.css'
+})
+export class ResultatComponent {
+
+}
