a1203518b4f534f42cfd044c6170f358faae45b1
youssef eddib <eddiby79@gmail.com>
2025-04-17 17:55:29 +0200

    creation la page categorie

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -2,11 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  standalone: true,
-  imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-
+  // يمكنك إضافة أي متغيرات أو دوال هنا إذا كنت بحاجة إليها في شريط التنقل.
 }
